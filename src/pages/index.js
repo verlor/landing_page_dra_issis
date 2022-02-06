d2b5162072600ed70ada78e8371c852fde47346c
Paola Peralta <peraltavianey@gmail.com>
2022-02-05 19:05:04 -0600

    cambio de nombre

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ const IndexPage = () => {
            <nav className="flex sm:justify-between justify-around items-center bg-green-900 lg:px-20 sm:px-6 py-8 text-gray-100">
                <h3 className={logoStyle}>HealthRoom</h3>
                <ul className="flex">
-                   <li className={linkStyle}><Link to="/">Home</Link></li>
+                   <li className={linkStyle}><Link to="/">Casa</Link></li>
                    <li className={linkStyle}><Link to="/contact">Contact</Link></li>
                    <li className={linkStyle}><Link to="/about">About</Link></li>
                </ul>
